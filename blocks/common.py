8f1642adfade27c5626781d6839c80fef04b7f85
Afshin Bozorgpour <afshinbigboy@gmail.com>
2024-12-16 22:40:20 +0100

    added baesmodule

--- blocks/common.py
+++ blocks/common.py
@@ -2,6 +2,23 @@ import torch
 import torch.nn as nn
 from einops import rearrange
 from timm.models.layers import DropPath, to_2tuple, trunc_normal_
+import math
+
+
+class BaseModule(nn.Module):
+    def _init_weights(self, m: nn.Module):
+        for name, p in m.named_parameters():
+            if name in ["out_proj.weight"]:
+                p = p.clone().detach_() # fake init, just to keep the seed ....
+                nn.init.kaiming_uniform_(p, a=math.sqrt(5))
+
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
 
 
 class LayerNormProxy(nn.Module):
